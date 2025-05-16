dec5b079ac83188fd7bc4b5bc26555cbc71f4b78
nemo720 <quandoan.140203@gmail.com>
2025-05-16 17:24:11 +0700

    Upload/Update Course Image

--- Backend/src/models/Course.js
+++ Backend/src/models/Course.js
@@ -18,6 +18,10 @@ const CourseSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Business',
     required: true
+  },
+  image: {
+    type: String,
+    default: 'default-course-image.jpg' // Default image path
   }
 }, {
   timestamps: true
